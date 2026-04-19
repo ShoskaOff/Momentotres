727693cb18cb8dbd0710fd2987f4dc0fc7e6165e
Camilo Andres Hernadez Gutierrez <hgcamilo94@gmail.com>
2026-04-19 16:35:56 -0500

    Se añadio responsive y tema claro/oscuro en tratamientos

--- Frontend/js/tema.js
+++ Frontend/js/tema.js
@@ -2,23 +2,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 const boton = document.getElementById("theme-toggle");
 
-boton.addEventListener("click", ()=>{
-
-document.body.classList.toggle("dark-mode");
-
-if(document.body.classList.contains("dark-mode")){
-    boton.textContent = "☀️";
-    localStorage.setItem("tema","oscuro");
-}else{
-    boton.textContent = "🌙";
-    localStorage.setItem("tema","claro");
-}
-
-});
-
 if(localStorage.getItem("tema") === "oscuro"){
     document.body.classList.add("dark-mode");
-    boton.textContent = "☀️";
+
+    if(boton){
+        boton.textContent = "☀️";
+    }
+}else{
+    if(boton){
+        boton.textContent = "🌙";
+    }
+}
+
+if(boton){
+    boton.addEventListener("click", ()=>{
+
+        document.body.classList.toggle("dark-mode");
+
+        if(document.body.classList.contains("dark-mode")){
+            boton.textContent = "☀️";
+            localStorage.setItem("tema","oscuro");
+        }else{
+            boton.textContent = "🌙";
+            localStorage.setItem("tema","claro");
+        }
+
+    });
 }
 
 });
