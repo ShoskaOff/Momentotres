0e8fcdc666601a3c7ceb8a8210698124f2a21f32
laura <bamgueroorejuelalauradaniela@gmail.com>
2026-04-24 18:06:04 -0500

    probando conexion 2

--- Frontend/js/servicios.js
+++ Frontend/js/servicios.js
@@ -2,15 +2,14 @@ const supabaseUrl = "https://jdofaujfqsyiwauwttcd.supabase.co";
 const supabaseKey = "sb_publishable_qS04TxbN_5PwBfJnAFS2Yg_L-NrgNLn";
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-async function prueba() {
-    const { data, error } = await supabase
-        .from("servicios")
-        .select("*");
 
-    console.log(data, error);
+async function testConexion() {
+    const { data, error } = await supabase.from("servicios").select("*");
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
 }
 
-prueba();
+testConexion();
 function verDetalle(tipo) {
     const t = tratamientos[tipo];
     document.getElementById("titulo").textContent = t.titulo;
